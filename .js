fff061a788314f88483b4384fc9c0b800fea5560
EmeraldNZ <ahyuancy@gmail.com>
2018-12-31 13:07:23 +1300

    Update .js

--- .js
+++ .js
@@ -1,3 +1,5 @@
+/***************   Solution #1   ***********************************************************************************************/
+
 function fearNotLetter(str) {
    for(let i = 0; i < str.length-1; i++){
       if(str.charCodeAt(i+1) - str.charCodeAt(i) !== 1){
@@ -7,6 +9,20 @@ function fearNotLetter(str) {
    return undefined;   
 }
 fearNotLetter("abcdeg"); // return "f"
+
+
+ 
+/***************   Solution #2   ***********************************************************************************************/
+
+function fearNotLetter(str){
+    for(let i = 0; i < str.length; i++){
+        if (str.charCodeAt(i) !== str.charCodeAt(0) + i) { //  If the UTF-16 code of current character is not equal to first character's code + its index, 
+                                                           //  hence the character at that index has been escaped 
+            return String.fromCharCode(str.charCodeAt(i) - 1);
+        }
+    }
+    return undefined;
+}
 
 
 
